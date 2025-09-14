4724bfb3d7518484923b64da2b3dabed762b5f44
rptetzloff <ray@raytetzloff.com>
2025-09-14 14:11:00 -0500

    Fix Vite entry module resolution

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  root: '.',
   base: '/',
-  publicDir: 'public',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
     sourcemap: false,
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      },
       output: {
         manualChunks: {
           vendor: ['vue']
         }
       }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
     }
   },
   server: {
