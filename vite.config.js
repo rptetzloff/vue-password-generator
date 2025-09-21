77fe32fa724898f1fda812efe16f6e676663a41e
rptetzloff <ray@raytetzloff.com>
2025-09-21 12:25:38 -0500

    Debug and fix Vue import issue

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,15 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: './',
   build: {
-    target: 'es2015',
     rollupOptions: {
+      external: [],
       output: {
-        format: 'iife',
-        inlineDynamicImports: true,
-        manualChunks: undefined
+        format: 'umd',
+        name: 'VuePasswordGenerator',
+        globals: {
+          vue: 'Vue'
+        }
       }
     }
   }
